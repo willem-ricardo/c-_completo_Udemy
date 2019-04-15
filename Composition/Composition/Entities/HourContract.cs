d2a47166522b17394405fb452281120000d9515c
Willem L. Ricardo <wlricardo@alu.ufc.br>
2019-04-15 11:47:16 -0300

    Criação das classes Department e HourContract

---
+++ Composition/Composition/Entities/HourContract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hour { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        HourContract(DateTime date, double value, int hour)
+        {
+            Date = date;
+            ValuePerHour = value;
+            Hour = hour;
+        }
+
+    }
+}
